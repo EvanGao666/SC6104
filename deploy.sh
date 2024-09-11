623f594e152267e5ba466f729cd2c20e0793b988
EvanGao666 <evan_gaoyf@outlook.com>
2024-09-11 23:01:52 +0800

    change

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 yarn install && yarn hardhat run ignition/modules/deploy.js --network sepolia
 # 构建并启动前端
-cd frontend && yarn install && yarn build && yarn start
+cd frontend && yarn cache clean && yarn install && yarn build && yarn start 
